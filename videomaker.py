551d5ad004021a5392ec7ec4fc172cf351031068
Murillo <murillo.bianconi>
2019-06-20 18:35:25 -0300

    add ffmpeg_subprocess

--- videomaker.py
+++ videomaker.py
@@ -16,14 +16,11 @@ def get_args():
     ap.add_argument("-ie", "--imageext", required=False, default='jpg', help="Image extension name. default is 'png'.")
     ap.add_argument("-me", "--musicext", required=False, default='mp3', help="Music extension name. default is 'mp3'.")
     ap.add_argument("-O", "--output", required=False, default='./output', help="Output Video folder")
+    ap.add_argument("-ve", "--videoext", required=False, default='mp4', help="Output Video Fomart")
     ap.add_argument("-v","--verbose", help="increase output verbosity", action="store_true")
 
     args = vars(ap.parse_args())
     return args
-
-# Define the codec
-def get_codec():
-    return cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
 
 # Get files from a folder that match the extension
 def get_files(folder, ext):
@@ -38,41 +35,16 @@ def get_files(folder, ext):
 # image and music = full path to files
 # output = full path of the new video (path/to/folder/video_name.mp4)
 #
-def create_video(image, music, output, fps=1):
-    logging.info("Creating Video: image: %s + audio: %s @ %s" % (image, music, output))
-    img = cv2.imread(image)
-    height, width, layers = img.shape
-    size = (width,height)
-    logging.debug("Vide Size: %s" % str(size))
-
-    
-    out = cv2.VideoWriter(output,get_codec(), fps, size)
-    logging.debug("VideoWriter: %s" % out)
-
-    # find the duration of the music and put a frame of the video
-    # vor every second of it
-    audio_length = get_music_length(music)
-    logging.debug("Video Lenght: %s seconds" % audio_length)
-
-    iterations = int(audio_length)
-    for i in range(iterations):
-        out.write(img)
-    out.release()
-
-    add_audio(output, music)
-
-def add_audio(video_file, audio_file):
-    # add audio to the original video, trim either the audio or video depends on which one is longer
-    subprocess.call(['ffmpeg', '-i', video_file, '-i', audio_file, '-shortest', '-c:v', 'copy', '-c:a', 'mp3','-b:a', '256k', '-y', video_file+"_with_audio.mp4"]) 
-
-# get the music duration in seconds
-def get_music_length(music):
-    mp3_file = MP3(music)
-    return mp3_file.info.length
+def create_video(image, music, video_ext ,location_folder):
+    output = os.path.join(location_folder, get_video_name(image, music))
+    if not video_ext.startswith('.'):
+        video_ext = '.' + video_ext
+    subprocess.call(["ffmpeg", "-loop","1" ,"-y","-i", image, "-i", music, "-acodec", "copy", "-vcodec", "mpeg4", "-shortest", output+video_ext])
 
 # function to create the video name, now its the exactly time of its creation
 def get_video_name(image, music):
-    return str(datetime.today().timestamp()) + ".mp4"
+    name, ext = os.path.basename(music).split('.')
+    return name
 
 
 # Main function
@@ -90,6 +62,7 @@ def main():
     music_folder = args['musicfolder']
     music_ext = args['musicext']
     output_folder = args['output']
+    video_extension = args['videoext']
 
     # all images from the folder
     images = get_files(folder=image_folder, ext=image_ext)
@@ -101,8 +74,7 @@ def main():
     
     # create a video made of 1 image with  soundtrack made of 1 music
     for image, music in zip(images, musics):
-        location = os.path.join(output_folder, get_video_name(image, music))    
-        create_video(image, music, location)
+        create_video(image, music, video_extension ,output_folder)
 
 if __name__=="__main__":
     main()
