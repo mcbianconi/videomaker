2a8d3e84a8a9d7c7a47d8a54bba8ec4324205819
Murillo <murillo.bianconi@gmail.com>
2019-06-27 16:43:29 -0300

    Adicionando Glob

--- videomaker.py
+++ videomaker.py
@@ -7,6 +7,8 @@ from mutagen.mp3 import MP3
 from datetime import datetime
 import logging
 import subprocess
+import PIL.Image
+import numpy as np
 
 def get_args():
     # Construct the argument parser and parse the arguments
@@ -55,7 +57,7 @@ def main():
     if args['verbose']:
         logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.ERROR)
+        logging.basicConfig(level=logging.DEBUG)
 
     image_ext = args['imageext']
     image_folder = args['imagefolder']
@@ -67,6 +69,10 @@ def main():
     # all images from the folder
     images = get_files(folder=image_folder, ext=image_ext)
     logging.debug("Found %s images on %s" % (len(images), image_folder))
+    
+    cimages = []
+    for f in build_images(image_folder, image_ext):
+        cimages.append(f)
 
     # all music from the folder
     musics = get_files(folder=music_folder, ext=music_ext)
@@ -75,6 +81,28 @@ def main():
     # create a video made of 1 image with  soundtrack made of 1 music
     for image, music in zip(images, musics):
         create_video(image, music, video_extension ,output_folder)
+
+def build_images(basedir, ext):
+    for root, dirs, fnames in os.walk(basedir):
+        imgs = []
+        for f in fnames:
+            if f.endswith(ext):
+                imgs.append(PIL.Image.open(os.path.join(root, f)))
+        if len(imgs) == 1:
+            yield imgs[0]
+            
+        min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
+        img_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )                
+        dest_dir = os.path.join(root,'after')
+        if not os.path.exists(dest_dir):
+            logging.info('Criando Diretorio: %s' % dest_dir)
+            os.mkdir(dest_dir)
+        
+        img_name = os.path.join(dest_dir, 'combined.' + ext) 
+        logging.info('Salvando Imagem: %s' % img_name)
+        img_comb = PIL.Image.fromarray( img_comb)
+        img_comb.save(img_name)
+        yield img_name
 
 if __name__=="__main__":
     main()
